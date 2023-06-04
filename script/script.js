2a34da3c815df41ccb40e9c4b5fe25a2dfc58716
AlexT97x <alextinella@gmail.com>
2023-06-04 17:34:40 +0200

    fine esercizio

--- script/script.js
+++ script/script.js
@@ -1,35 +1,110 @@
+"use strict";
 
-function createGrid() {
-    let cell = document.createElement('div');
-    cell.classList.add('cell');
-    return cell;
+function generateRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-const play_btn = document.getElementById('play_btn');
+function fillExplosiveCell(array_explosive_tnt, max) {
+       
+    let control = false;
+    let random;
 
-play_btn.addEventListener('click', function(){
-    const grid = document.getElementById('grid');
-    grid.innerHTML = '';
-
-    const maxDifficulty = 100;
-
-    for (let i = 1; i <= maxDifficulty; i++) {
-    let cell = createGrid();
-    cell.innerHTML = i;
-
-    cell.addEventListener('click', function(){
+    while (control === false) {
+        random = generateRandomNumber(1, max);
         
-        this.classList.add('clicked_cell');
-            console.log(this.innerHTML);
-        });
-
-        grid.append(cell);
+        if (!array_explosive_tnt.includes[random]) {
+            control = true;
+        }
+    }
+    return random;
 }
-});
+
+function newGame() {
+    const grid = document.getElementById('grid'); 
+    const array_explosive_tnt = []; 
+
+    
+    const difficulty_level = parseInt(document.getElementById('difficulty_level').value); 
+
+    
+    let cellNumber;
+    switch (difficulty_level) {
+        case 0:
+        cellNumber = 100;
+        break;
+        case 1:
+        cellNumber = 81;
+        break;
+        case 2:
+        cellNumber = 49;
+        break;
+    }
+
+    for (let i = 0; i < 16; i++) {
+        let number = fillExplosiveCell(array_explosive_tnt, cellNumber); 
+        array_explosive_tnt.push(number);
+    }
+    console.log(array_explosive_tnt)
+
+    grid.innerHTML = ''; 
+
+    createCell(cellNumber, array_explosive_tnt); 
+}
 
 
+function createCell(cellx, array_explosive_tnt) { 
+
+    
+    let gameTheEnd = false;
+
+    
+    let score = 0;
+
+    
+    for (let i = 0; i < cellx; i++) {
+
+        const cell = document.createElement('div');
+        let cellForRow = Math.sqrt(cellx);
+
+        cell.classList.add('cell');
+        cell.style.width =`calc(100% / ${cellForRow})`;
+        cell.style.height = cell.style.width;
+        cell.innerHTML = i + 1;
+
+        cell.addEventListener('click', function(){
+
+            if (gameTheEnd === false) {
+
+                if(!array_explosive_tnt.includes(parseInt(this.innerText))){
+                    this.classList.add('clicked_cell');
+                    score++;
+                    console.log(score);
+                }
+                else{
+                    this.classList.add('cell_exploded');
+                    gameTheEnd = true;
+
+                    
+                    let cell_all = document.querySelectorAll('.cell')
+                    for (let i = 0; i < cell_all.length; i++){
+                        if (array_explosive_tnt.includes(i+1)){
+                            cell_all[i].classList.add('cell_explode'); 
+                        }
+                    }
+
+                    alert(`Il tuo punteggio è: ${score} . Premi Play e prova a fare di meglio! :)`);
+                   
+
+                }
+            }
+        })
+        grid.append(cell);
+    }
+}
 
 
-
-
-
+btn.addEventListener('click', function(){
+    const btn = document.getElementById('btn'); 
+    alert("Benvenuto in campo minato, scegli il livello di difficoltà e prova a fare il punteggio migliore :)")
+    newGame();
+})
